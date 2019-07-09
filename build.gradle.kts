2fe0a25055913268f53ea702aac6f3b31052fdf2
Sagi Antebi <sagi.antebi@fyber.com>
2019-07-09 13:36:47 +0300

    Added bintray publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.3.31"
+    id("com.jfrog.bintray") version "1.7"
+    maven
+    `maven-publish`
 }
 
 group = "com.sagiantebi"
@@ -41,4 +44,34 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+            from(components["java"])
+        }
+    }
+}
+
+bintray {
+    user = if (project.hasProperty("bintrayUser")) { project.property("bintrayUser")  as String } else { System.getenv("BINTRAY_USER") ?: "" }
+    key = if (project.hasProperty("bintrayApiKey")) { project.property("bintrayApiKey") as String } else { System.getenv("BINTRAY_API_KEY") ?: "" }
+    setConfigurations("archives")
+    with(pkg) {
+        dryRun = true
+        repo = "mvn"
+        name = project.name
+        userOrg = user
+        setLicenses("MIT")
+        vcsUrl = "https://github.com/sagiantebi/failfast-repositories"
+        with(version) {
+            name = project.name
+            desc = ""
+            vcsTag = project.version.toString() //assume version is git-tagged.
+        }
+    }
 }
